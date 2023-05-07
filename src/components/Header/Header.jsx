624cfc302af7ed41dae48e64c405ff8fc6a68fbf
Temitope <themyturpeokesanya@gmail.com>
2023-05-07 16:18:54 +0100

    search filter implementation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import logo from '../../assets/images/eco-logo.png'
 import { NavLink } from 'react-router-dom'
 import userIcon from '../../assets/images/user-icon.png'
 import { motion } from 'framer-motion'
+import { useSelector } from 'react-redux'
 
 const nav__links = [
   {
@@ -23,6 +24,7 @@ const nav__links = [
 const Header = () => {
  
   const headerRef = useRef(null)
+  const totalQuantity = useSelector(state => state.cart.totalQuantity)
 
   const menuRef = useRef(null)
 
@@ -73,7 +75,7 @@ const Header = () => {
             </span>
             <span className="cart__icon">
               <i class='ri-shopping-bag-line'></i>
-              <span className="badge">2</span>
+              <span className="badge">{totalQuantity}</span>
             </span>
             <span>
               <motion.img whileTap={{scale: 1.2}} src={userIcon} alt="user" />
